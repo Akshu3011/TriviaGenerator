deaaebf23507ecb533877f48680fe38dd5e6514a
Akshata Kumbhar <105023659+Akshu3011@users.noreply.github.com>
2022-06-28 21:58:24 -0700

    javascript quedocode ready

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+//TODO:Store the username in ls
+    //TODO:Access the username on quiz page
+//TODO:generate a quiz
+    //TODO:have a questionbank
+    //TODO:show questions
+//TODO:show results on submit button
+    //TODO:find selected answer and and compare with defined correct answer
+    //TODO:save the count of correct and wrong nswers
+//TODO:Show the overall results on timer end
+//TODO:put a timer for quiz
+//TODO:Manage edge cases:
+    //TODO:User starts the quiz inbetween
+    //TODO:quiz should halt at timer stop 
+    
